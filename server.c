1d92110b7a547ec5ebb6f2535d19ea25c739f930
Andrés Montoya <montoyaw1@gmail.com>
2024-04-28 17:17:58 -0600

    Server socket creation

---
+++ server.c
@@ -0,0 +1,75 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <signal.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
+
+int srv_socket_descript = 0;
+
+void exit_service(int sig) {
+    printf("\nShutting down...\n");
+    exit(EXIT_SUCCESS);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Provide a port number!\n");
+        printf("Usage: %s <port>\n", argv[0]);
+        return 1;
+    }
+
+    // Save the port number
+    int port = atoi(argv[1]);
+
+    signal(SIGINT, exit_service);
+
+    // Socket creation
+    srv_socket_descript = socket(AF_INET, SOCK_STREAM, 0);
+    // Check if the socket is created successfully
+    if (srv_socket_descript == -1) {
+        printf("Socket creation failed!\n");
+        exit(EXIT_FAILURE);
+    } else{
+        printf("Socket created successfully!\n");
+    }
+
+    // Save the server address and client address
+    struct sockaddr_in srv_address, client_address;
+    int srv_addr_len = sizeof(srv_address);
+    int cli_addr_len = sizeof(client_address);
+
+    // Initialize the server and client address
+    memset(&srv_address, 0, srv_addr_len);
+    memset(&client_address, 0, cli_addr_len);
+
+    // Set the server address and port
+    srv_address.sin_family = AF_INET;
+    srv_address.sin_addr.s_addr = INADDR_ANY;
+    srv_address.sin_port = htons(port);
+
+    // Bind the socket to the server address
+    if (bind(srv_socket_descript, (struct sockaddr *) &srv_address, srv_addr_len) == -1) {
+        printf("Binding failed!\n");
+        exit(EXIT_FAILURE);
+    } else {
+        printf("Binding successful!\n");
+    }
+
+    // Listen for incoming connections
+    if (listen(srv_socket_descript, 5) == -1) {
+        printf("Listening failed!\n");
+        exit(EXIT_FAILURE);
+    } else {
+        printf("Socket is listening...\n");
+    }
+
+    // Configure the client ip and port
+    getsockname(srv_socket_descript, (struct sockaddr *) &srv_address, (socklen_t *) &srv_addr_len);
+    printf("Server started on %s:%d\n", inet_ntoa(srv_address.sin_addr), ntohs(srv_address.sin_port));
+
+}
